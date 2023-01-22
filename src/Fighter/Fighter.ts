1658bf338cfda24d2dec00faf544090c9a8364bc
Raypcte <rayanevalak@gmail.com>
2023-01-22 15:25:42 -0300

    req7

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,14 +1,14 @@
-import { EnergyType } from '../Energy';
+import Energy from '../Energy';
 
-interface Figther{
-  lifePoints: number,
-  strength: number,
-  defense: number,
-  energy?: EnergyType, 
-  attack: (enemy: Figther) => void,
-  special?: (enemy: Figther) => void,
-  levelUp: () => void,
-  receiveDamage: (attackPoints: number) => number,
+interface Figther {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy; 
+  attack: (enemy: Figther) => void;
+  special?: (enemy: Figther) => void;
+  levelUp: () => void;
+  receiveDamage: (attackPoints: number) => number;
 }
 
 export default Figther;
